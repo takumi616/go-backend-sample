cf1e11e8edcee91c63c4d0b67b1743c5d5b2057a
takumi616 <ohohyeah321@icloud.com>
2025-07-24 23:52:04 +0900

    Set up github actions workflow

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthCheck(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	rec := httptest.NewRecorder()
+
+	healthCheck(rec, req)
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	expected := "HTTP server works correctly"
+	body := rec.Body.String()
+	assert.Equal(t, expected, body)
+}
